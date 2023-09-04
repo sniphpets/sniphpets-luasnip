50d823c9c1c33316e47618f9a869a2cfa745b41c
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-09-05 01:37:04 +0300

    Improve snippets

--- snippets/common/php.lua
+++ snippets/common/php.lua
@@ -1,5 +1,6 @@
 local config = require('sniphpets-luasnip').config
 local common = require('sniphpets-luasnip.common')
+local conds_expand = require('luasnip.extras.conditions.expand')
 local prefix = config.common.prefix
 local basename = common.basename
 local namespace = common.namespace
@@ -7,7 +8,7 @@ local file_header = common.file_header()
 
 return {
   s(
-    prefix .. 'class',
+    { trig = prefix .. 'class', name = 'Class definition', dscr = 'Class definition' },
     fmt(file_header .. [[
 namespace @#;
 
@@ -15,6 +16,7 @@ class @#
 {
     @#
 }
-    ]], { f(namespace), f(basename), i(0) }, { delimiters = '@#' })
+    ]], { f(namespace), f(basename), i(0) }, { delimiters = '@#' }),
+    { condition = conds_expand.line_begin }
   ),
 }
--- snippets/phpunit/php.lua
+++ snippets/phpunit/php.lua
@@ -1,6 +1,7 @@
 local config = require('sniphpets-luasnip').config
 local common = require('sniphpets-luasnip.common')
 local phpunit = require('sniphpets-luasnip.phpunit')
+local conds_expand = require('luasnip.extras.conditions.expand')
 local prefix = config.phpunit.prefix
 local basename = common.basename
 local namespace = phpunit.namespace
@@ -9,7 +10,7 @@ local file_header = common.file_header()
 
 return {
   s(
-    prefix .. 'case',
+    { trig = prefix .. 'case', name = 'PHPUnit: test case', dscr = 'PHPUnit: test case' },
     fmt(file_header .. [[
 namespace @#;
 
@@ -20,6 +21,7 @@ class @# extends TestCase
 {
     @#
 }
-  ]], { f(namespace), f(sut), f(basename), i(0) }, { delimiters = '@#' })
+  ]], { f(namespace), f(sut), f(basename), i(0) }, { delimiters = '@#' }),
+    { condition = conds_expand.line_begin }
   ),
 }

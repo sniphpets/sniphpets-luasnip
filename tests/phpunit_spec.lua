625fadfb0ff17134a97acdebfa2469b1b43785a8
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2023-09-04 18:52:11 +0300

    Add more tests for phpunit

--- tests/phpunit_spec.lua
+++ tests/phpunit_spec.lua
@@ -1,6 +1,27 @@
 local phpunit = require('sniphpets-luasnip.phpunit')
+local config = require('sniphpets-luasnip').config
 
 describe('sniphpets-luasnip.phpunit', function()
+  before_each(function()
+    config.namespace_prefix = 'App'
+    config.phpunit.namespace_prefix = 'App\\Tests'
+    config.phpunit.test_suffix = 'Test'
+  end)
+
+  describe('namespace()', function()
+    local namespace = phpunit.namespace
+
+    local test_filepath = ''
+    require('sniphpets-luasnip.common').filepath = function()
+      return test_filepath
+    end
+
+    it([[returns test class namespace]], function()
+      test_filepath = '/home/user/projects/symfony-demo/tests/Utils/ValidatorTest.php'
+      assert.are.same('App\\Tests\\Utils', namespace())
+    end)
+  end)
+
   describe('sut()', function()
     local sut = phpunit.sut
 
@@ -9,13 +30,18 @@ describe('sniphpets-luasnip.phpunit', function()
       return test_fqn
     end
 
-    it("returns SUT's fully qualified name", function()
+    it([[returns SUT's fully qualified name]], function()
       test_fqn = 'App\\Tests\\Utils\\ValidatorTest'
-
       assert.are.same('App\\Utils\\Validator', sut())
+
+      test_fqn = 'Tests\\Utils\\ValidatorSpec'
+      config.namespace_prefix = 'AppBundle'
+      config.phpunit.namespace_prefix = 'Tests'
+      config.phpunit.test_suffix = 'Spec'
+      assert.are.same('AppBundle\\Utils\\Validator', sut())
     end)
 
-    it('removes "Unit" from SUT\'s fully qualified name', function()
+    it([[removes "Unit" from SUT's fully qualified name]], function()
       test_fqn = 'App\\Tests\\Unit\\Utils\\ValidatorTest'
 
       assert.are.same('App\\Utils\\Validator', sut())

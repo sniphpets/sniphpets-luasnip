cae303933d5f61a53d9dcdd7466ff28f40ec6d16
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2024-09-12 04:15:17 +0300

    Add more Joomla snippets

--- snippets/joomla/php.lua
+++ snippets/joomla/php.lua
@@ -7,6 +7,10 @@ local base = common.base
 local namespace = joomla.namespace
 local file_header = common.file_header()
 
+local function table_alias()
+  return common.basename():sub(1, 1):lower()
+end
+
 return {
   s(
     {
@@ -60,20 +64,35 @@ use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
       dscr = 'Joomla: Item model class definition',
     },
     fmt(file_header .. [[
-namespace @#;
+namespace @!;
 
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\Model\ItemModel;
+use Joomla\Database\ParameterType;
 
-@# extends ItemModel
+@! extends ItemModel
 {
     public function getItem($pk = null): object
     {
-        @#
+        $pk = $pk ?? (int) $this->getState('item.id');
+
+        $db = $this->getDatabase();
+        $query = $db->getQuery(true);
+
+        $query->select('*')
+            ->from($db->quoteName('#__@!', '@!'))
+            ->where($db->quoteName('@!.id') . ' = :pk')
+            ->bind(':pk', $pk, ParameterType::INTEGER);
+
+        $db->setQuery($query);
+
+        $item = $db->loadObject();
+
+        return $item;
     }
 }
-  ]], { f(namespace), f(base), i(0) }, { delimiters = '@#' }),
+  ]], { f(namespace), f(base), i(0), f(table_alias), f(table_alias) }, { delimiters = '@!' }),
     { condition = conds_expand.line_begin }
   ),
 
@@ -84,25 +103,85 @@ use Joomla\CMS\MVC\Model\ItemModel;
       dscr = 'Joomla: List model class definition',
     },
     fmt(file_header .. [[
-namespace @#;
+namespace @!;
 
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\Model\ListModel;
 use Joomla\Database\DatabaseQuery;
 
-@# extends ListModel
+@! extends ListModel
 {
     protected function getListQuery(): DatabaseQuery|string
     {
-        $query = $this->getDatabase()->getQuery(true);
+        $db = $this->getDatabase();
+        $query = $db->getQuery(true);
 
-        @#
+        $query->select('*')
+            ->from($db->quoteName('#__@!', '@!'))
+            ->where(@!);
 
         return $query;
     }
 }
-  ]], { f(namespace), f(base), i(0) }, { delimiters = '@#' }),
+  ]], { f(namespace), f(base), i(1), f(table_alias), i(0) }, { delimiters = '@!' }),
     { condition = conds_expand.line_begin }
   ),
+
+  s(
+    {
+      trig = prefix .. 'template',
+      name = 'Joomla: PHP file',
+      dscr = 'Joomla: PHP file with _JEXEC',
+    },
+    fmt(file_header .. [[
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+
+?>
+
+@#
+  ]], { i(0) }, { delimiters = '@#' }),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s(
+    {
+      trig = prefix .. 'query',
+      name = 'Joomla: Query',
+      dscr = 'Joomla: SQL query',
+    },
+    fmt(
+      [[
+$db = $this->getDatabase();
+$query = $db->getQuery(true);
+
+$query->@#
+  ]],
+      { i(0) },
+      { delimiters = '@#' }
+    ),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s({
+    trig = prefix .. 'qv',
+    name = 'Joomla: Quote value',
+    dscr = 'Joomla: Quote value',
+  }, fmt([[$db->quote('@#')@#]], { i(1), i(0) }, { delimiters = '@#' }), {}),
+
+  s({
+    trig = prefix .. 'qn',
+    name = 'Joomla: Quote name',
+    dscr = 'Joomla: Quote name',
+  }, fmt([[$db->quoteName('@#')@#]], { i(1), i(0) }, { delimiters = '@#' }), {}),
+
+  s({
+    trig = prefix .. 'route',
+    name = 'Joomla: Route',
+    dscr = 'Joomla: Route URL',
+  }, fmt([[Route::_('index.php?option=com_@#')@#]], { i(1), i(0) }, { delimiters = '@#' }), {}),
 }

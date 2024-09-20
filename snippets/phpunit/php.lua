2427fccf4c647e243e4d1418bddc1aa38ac36e33
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2024-09-20 13:45:06 +0300

    [PHPUnit] Add more snippets

--- snippets/phpunit/php.lua
+++ snippets/phpunit/php.lua
@@ -27,7 +27,11 @@ namespace @#;
   ),
 
   s(
-    { trig = prefix .. 'case', name = 'PHPUnit: Test case', dscr = 'PHPUnit: Test case' },
+    {
+      trig = prefix .. 'case',
+      name = 'PHPUnit: Test case',
+      dscr = 'PHPUnit: Test case',
+    },
     fmt(file_header .. [[
 namespace @#;
 
@@ -39,6 +43,179 @@ use @#;
     @#
 }
   ]], { f(namespace), f(sut), f(base), i(0) }, { delimiters = '@#' }),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s(
+    {
+      trig = prefix .. 'test',
+      name = 'PHPUnit: Test method',
+      dscr = 'PHPUnit: Test method',
+    },
+    fmt(
+      [[
+public function test@#(): void
+{
+    @#
+}
+  ]],
+      { i(1), i(0) },
+      { delimiters = '@#' }
+    ),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s(
+    {
+      trig = prefix .. 'a',
+      name = 'PHPUnit: Assert',
+      dscr = 'PHPUnit: Assert',
+    },
+    fmt(
+      [[
+$this->assert@#(@#);
+  ]],
+      { i(1), i(2) },
+      { delimiters = '@#' }
+    ),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s(
+    {
+      trig = prefix .. 'eq',
+      name = 'PHPUnit: Assert equals',
+      dscr = 'PHPUnit: Assert equals',
+    },
+    fmt(
+      [[
+$this->assertEquals(@#, @#);
+  ]],
+      { i(1), i(2) },
+      { delimiters = '@#' }
+    ),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s(
+    {
+      trig = prefix .. 'same',
+      name = 'PHPUnit: Assert same',
+      dscr = 'PHPUnit: Assert same',
+    },
+    fmt(
+      [[
+$this->assertSame(@#, @#);
+  ]],
+      { i(1), i(2) },
+      { delimiters = '@#' }
+    ),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s(
+    {
+      trig = prefix .. 'exception',
+      name = 'PHPUnit: Expect exception',
+      dscr = 'PHPUnit: Expect exception',
+    },
+    fmt(
+      [[
+$this->expectException(@#::class);
+$this->expectExceptionMessage('@#');
+    ]],
+      { i(1), i(2) },
+      { delimiters = '@#' }
+    ),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s(
+    {
+      trig = prefix .. 'pro',
+      name = 'PHPUnit: Prophesize',
+      dscr = 'PHPUnit: Prophesize',
+    },
+    fmt(
+      [[
+$@# = $this->prophesize(@#::class);
+    ]],
+      { i(1), i(2) },
+      { delimiters = '@#' }
+    ),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s(
+    {
+      trig = prefix .. 'mock',
+      name = 'PHPUnit: Mock',
+      dscr = 'PHPUnit: Mock',
+    },
+    fmt(
+      [[
+$@# = $this->createMock(@#::class);
+    ]],
+      { i(1), i(2) },
+      { delimiters = '@#' }
+    ),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s(
+    {
+      trig = prefix .. 'stub',
+      name = 'PHPUnit: Stub',
+      dscr = 'PHPUnit: Stub',
+    },
+    fmt(
+      [[
+$@# = $this->createStub(@#::class);
+$@#->method('@#')
+    ->willReturn(@#);
+    ]],
+      { i(1), i(2), rep(1), i(3), i(4) },
+      { delimiters = '@#' }
+    ),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s(
+    {
+      trig = prefix .. 'fake',
+      name = 'PHPUnit: Fake',
+      dscr = 'PHPUnit: Fake',
+    },
+    fmt(
+      [[
+$@# = $this->createStub(@#::class);
+$@#->method('@#')
+    ->willReturnCallback(
+        function(@#) {
+            @#
+        }
+    );
+    ]],
+      { i(1), i(2), rep(1), i(3), i(4), i(0) },
+      { delimiters = '@#' }
+    ),
+    { condition = conds_expand.line_begin }
+  ),
+
+  s(
+    {
+      trig = prefix .. 'expects',
+      name = 'PHPUnit: Expects',
+      dscr = 'PHPUnit: Expects',
+    },
+    fmt(
+      [[
+$@#->expects($this->@#())
+    ->method('@#');
+    ]],
+      { i(1), i(2, 'once'), i(3) },
+      { delimiters = '@#' }
+    ),
     { condition = conds_expand.line_begin }
   ),
 }
